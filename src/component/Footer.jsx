7573015c78c84f5506322d2deb8d67a817d36fc9
Tayyab-Tech <tayyabtechnology@gmail.com>
2025-05-13 15:46:59 +0500

    footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import { FaPinterest, FaLinkedinIn, FaXbox   } from "react-icons/fa";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { MdFacebook } from "react-icons/md";
+import Link from 'next/link';
 function Footer() {
   return (
-    <div>
-      bna b lo bahi
-    </div>
+    <>
+      <div className='bg-black px-[60px] py-[20px] flex md:flex-row flex-col justify-between items-center gap-[30px]'>
+        <div>
+          © 2024 Kyubi Studio
+        </div>
+        <div>
+          <ul className='list-none flex items-center gap-[30px]'>
+            <li><Link href="#"><FaPinterest /></Link></li>
+            <li><Link href="#"><AiFillTwitterCircle /></Link></li>
+            <li><Link href="#"><FaSquareInstagram /></Link></li>
+            <li><Link href="#"><MdFacebook /></Link></li>
+            <li><Link href="#"><FaLinkedinIn /></Link></li>
+            <li><Link href="#"><FaXbox /></Link></li>
+          </ul>
+        </div>
+      </div>
+    </>
   )
 }
 
